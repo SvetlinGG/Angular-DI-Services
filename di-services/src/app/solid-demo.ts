941f4b302f84475ff9e5a9003797a80a98051b0b
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-07-27 16:20:37 +0200

    dependency injection

--- di-services/src/app/solid-demo.ts
+++ di-services/src/app/solid-demo.ts
@@ -1,71 +1,105 @@
-// Single Responsibility
+// // Single Responsibility
 
-class User {
-    firstName: string;
-    lastName: string;
-    address: string;
+// class User {
+//     firstName: string;
+//     lastName: string;
+//     address: string;
 
-    constructor(firstName: string, lastName: string, address: string,
-    ){
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.address = address;
-    }
+//     constructor(firstName: string, lastName: string, address: string,
+//     ){
+//         this.firstName = firstName;
+//         this.lastName = lastName;
+//         this.address = address;
+//     }
 
-    getDetails(){
-        console.log(`${this.firstName} ${this.lastName} is from ${this.address} `);
+//     getDetails(){
+//         console.log(`${this.firstName} ${this.lastName} is from ${this.address} `);
         
-    }
-}
+//     }
+// }
 
-class Wallet {
-    balance: number;
-    currency: string;
+// class Wallet {
+//     balance: number;
+//     currency: string;
 
-    constructor(balance: number, currency: string){
-        this.balance = balance;
-        this.currency = currency;
-    }
+//     constructor(balance: number, currency: string){
+//         this.balance = balance;
+//         this.currency = currency;
+//     }
 
-    saveMoney(){
+//     saveMoney(){
 
-    }
+//     }
 
-    removeMoney(){
+//     removeMoney(){
 
-    }
-}
+//     }
+// }
 
-interface Address {
-    str: string;
-    country: string;
-}
+// interface Address {
+//     str: string;
+//     country: string;
+// }
 
-interface WalletDetails {
-    type: string;
-    balance: number;
-    currency: string;
-}
-interface PersonPrivateDetails {
-    firstName: string;
-    lastName: string;
-}
+// interface WalletDetails {
+//     type: string;
+//     balance: number;
+//     currency: string;
+// }
+// interface PersonPrivateDetails {
+//     firstName: string;
+//     lastName: string;
+// }
 
-interface PersonPublicDetails extends PersonPrivateDetails, WalletDetails {
+// interface PersonPublicDetails extends PersonPrivateDetails, WalletDetails {
     
-    address: Address;
+//     address: Address;
     
 
 
-class Person implements PersonPrivateDetails {
-    firstName: string;
-    lastName: string;
+// class Person implements PersonPrivateDetails {
+//     firstName: string;
+//     lastName: string;
 
-    constructor(firstName: string, lastName: string){
-        this.firstName = firstName;
-        this.lastName = lastName;
-    }
+//     constructor(firstName: string, lastName: string){
+//         this.firstName = firstName;
+//         this.lastName = lastName;
+//     }
 
-}
+// }
 // Dependency Inversion is the RULE
 // Dependency Injection --> realization of the rule (Design Pattern)
+
+class Wallet {
+    balance = 0;
+
+    constructor(balance: number){
+        this.balance = balance;
+    }
+}
+
+class Car {
+    model: string;
+    make: string;
+    color: string;
+
+    constructor(model: string, make: string, color: string){
+        this.model = model;
+        this.make = make;
+        this.color = color;
+    }
+}
+
+class User {
+    name: string;
+    wallet: Wallet;
+    car: Car;
+
+    constructor(name: string, balance: number, model: string, make: string, color: string){
+        this.name = name;
+        this.wallet = new Wallet(balance);
+        this.car = new Car(model, make, color);
+    }
+}
+const userPesho = new User('Pesho', 500, 'VW', 'Polo', 'blue');
+const userMaria = new User('Maria', 400, 'Mercedes', 'AMG', 'black');
