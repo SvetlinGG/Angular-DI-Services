687487eeb2643fd0334896a396cb4b4825dc5fe9
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-08-10 22:13:31 +0200

    rout path

--- demo-standalone/src/app/app.routes.ts
+++ demo-standalone/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { UsersComponent } from './users/users.component';
+import { UserDetailsComponent } from './users/user-details/user-details.component';
 
 export const routes: Routes = [
     {path: "", component: HomeComponent},
-    {path: "users", component: UsersComponent}
+    {path: "users", component: UsersComponent},
+    {path: "users/details/:id", component: UserDetailsComponent},
 ];
