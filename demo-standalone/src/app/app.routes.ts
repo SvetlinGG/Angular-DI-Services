10bbe78de438784298c5bdaf64d620ef2d6c7645
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2025-08-10 22:26:27 +0200

    error page

--- demo-standalone/src/app/app.routes.ts
+++ demo-standalone/src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { UsersComponent } from './users/users.component';
 import { UserDetailsComponent } from './users/user-details/user-details.component';
+import { ErrorPageComponent } from './error-page/error-page.component';
 
 export const routes: Routes = [
     {path: "", redirectTo: '/home', pathMatch: 'full'},
     {path: 'home', component: HomeComponent},
     {path: "users", component: UsersComponent},
     {path: "users/details/:id", component: UserDetailsComponent},
+    {path: '**', component: ErrorPageComponent}
 ];
