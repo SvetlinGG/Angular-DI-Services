9bdfb6c0df4fd44d5f259b231a1f876d41513f69
SvetlinGarabedyan <146958421+SvetlinGG@users.noreply.github.com>
2026-03-23 22:55:07 +0100

    make refresh list button

--- demo-DI-services/src/app/user-list/user-list.component.ts
+++ demo-DI-services/src/app/user-list/user-list.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, Input } from '@angular/core';
 import { UserListItemComponent } from './user-list-item/user-list-item.component';
 import { SimpleUser } from '../types';
 
@@ -8,8 +8,16 @@ import { SimpleUser } from '../types';
   imports: [UserListItemComponent],
   templateUrl: './user-list.component.html',
   styleUrl: './user-list.component.css',
-  changeDetection: ChangeDetectionStrategy.Default,
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class UserListComponent {
   @Input('users') usersList: SimpleUser[] = [];
+
+  constructor(private cd: ChangeDetectorRef){}
+
+
+    refreshList(){
+      this.cd.detectChanges();
+    }
+  
 }
